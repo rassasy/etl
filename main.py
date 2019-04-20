b1c9ac9b8da02a7ea9c94b34c79ddb7bff1f1c9a
Brett Spradling <bspradling@godaddy.com>
2019-04-20 08:59:38 -0700

    Google Sheets Integration

---
+++ main.py
@@ -0,0 +1,12 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/bspradling/Desktop/rassasy.json', scope)
+
+gc = gspread.authorize(credentials)
+
+sheet = gc.open('Food Files')
+values = sheet.worksheet('Development').get_all_values()
